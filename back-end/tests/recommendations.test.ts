9918762d40f9f2aa418f91ca496d971c81bebe3e
rafaelarissa <rafaela.larissalj@gmail.com>
2022-05-07 21:01:35 -0300

    feat: set tests on POST /recommendations/:id/upvote

--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -4,13 +4,9 @@ import { prisma } from "../src/database.js";
 import { CreateRecommendationData } from "../src/services/recommendationsService.js";
 
 describe("Recommendations tests - POST /recommendations", () => {
-  beforeEach(async () => {
-    await prisma.$executeRaw`TRUNCATE TABLE "recommendations";`;
-  });
+  beforeEach(truncateRecommendations);
 
-  afterAll(async () => {
-    await prisma.$disconnect();
-  });
+  afterAll(disconnect);
 
   it("should return 201 given a valid body", async () => {
     const body: CreateRecommendationData = {
@@ -43,3 +39,45 @@ describe("Recommendations tests - POST /recommendations", () => {
     expect(response.status).toEqual(409);
   });
 });
+
+describe("Recommendations tests - POST /recommendations/:id/upvote", () => {
+  beforeEach(truncateRecommendations);
+
+  afterAll(disconnect);
+
+  it("should return 200 given valid id", async () => {
+    const body: CreateRecommendationData = {
+      name: "Joji Best Songs Collections - Joji Greatest Hits - Joji Songs Full Playlist - The Best Of Joji",
+      youtubeLink: "https://www.youtube.com/watch?v=81AShKbT_TY",
+    };
+
+    await supertest(app).post("/recommendations").send(body);
+    const recommendation = await supertest(app).get("/recommendations");
+    const id = recommendation.body[0].id;
+    console.log(id);
+
+    const response = await supertest(app)
+      .post(`/recommendations/${id}/upvote`)
+      .send();
+
+    expect(response.status).toEqual(200);
+  });
+
+  it("should return 404 if given unexisting recommendation", async () => {
+    const id = 0;
+
+    const response = await supertest(app)
+      .post(`/recommendations/${id}/upvote`)
+      .send();
+
+    expect(response.status).toEqual(404);
+  });
+});
+
+async function disconnect() {
+  await prisma.$disconnect();
+}
+
+async function truncateRecommendations() {
+  await prisma.$executeRaw`TRUNCATE TABLE "recommendations";`;
+}
