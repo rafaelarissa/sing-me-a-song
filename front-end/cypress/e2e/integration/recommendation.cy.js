6d8af51ff0d155775248a3e88bdd7de40237de5d
rafaela justino <rafaela.larissalj@gmail.com>
2022-07-28 22:16:50 -0300

    test: added a recommendation command file

--- front-end/cypress/e2e/integration/recommendation.cy.js
+++ front-end/cypress/e2e/integration/recommendation.cy.js
@@ -3,6 +3,10 @@
 import { faker } from "@faker-js/faker";
 
 describe("recomendation suit test", () => {
+  beforeEach(() => {
+    cy.request("POST", "http://localhost:5000/e2e/reset-database", {});
+  });
+
   it("should create a new recommendation given a valid input", () => {
     const recommendation = {
       name: faker.music.songName(),
@@ -23,5 +27,11 @@ describe("recomendation suit test", () => {
     cy.wait("@createRecommendation");
 
     cy.contains(recommendation.name);
+  });
+
+  it("add a vote point to a recommendation", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get('d[target="M5 3L0 9h3v4h4V9h3L5 3z"]');
   });
 });
